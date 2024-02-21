e22ea8428a2f7bc436750f48b2be9b9f2357bbe5
zwd <17326195691@163.com>
2024-02-21 14:21:53 +0800

    接口版本控制

--- apps/nest-app/src/app.controller.ts
+++ apps/nest-app/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: 'user',
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Version([VERSION_NEUTRAL, '1'])
+  findAll() {
+    // return this.appService.findAll();
+    return 'I am old one';
+  }
+
+  @Get()
+  @Version('2')
+  findAll2() {
+    return 'I am new one';
   }
 }
--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -1,8 +1,27 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 接口版本化管理
+  app.enableVersioning({
+    // defaultVersion: '1',
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
