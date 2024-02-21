bfec17d12b32966e156cb8b4db6ba1fdae7cb861
zwd <17326195691@163.com>
2024-02-21 14:44:30 +0800

    封装全局请求

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -10,6 +10,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +23,11 @@ async function bootstrap() {
     defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
   });
+
+  // 添加全局格式化拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 添加全局异常处理
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
   await app.listen(3000);
 }
